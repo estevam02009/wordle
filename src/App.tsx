2f92d2ee12048dcd48e4f5b4b154335f02911659
estevam02009 <manoel,neto53861@alunos.ufersa.edu.br>
2023-01-06 22:33:40 -0300

    create styled component Word entry control

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import React, { useEffect, useState } from 'react'
 import './App.css';
-import Letter from './components/Letter';
-import { AccuracyEnum } from './utilities/accuracy.util';
+//import Letter from './components/Letter';
+import { Word } from './components/Word';
+import { WordEntry } from './components/WordEntry';
+//import { AccuracyEnum } from './utilities/accuracy.util';
 
 function App() {
+
+  const [wordGuess, setWordGuess] = useState('')
+
   return (
     <div>
-      <Letter accuracy={AccuracyEnum.correct} position={0} value='R' />
-      <Letter accuracy={AccuracyEnum.doesNotExist} position={1} value='E' />
-      <Letter accuracy={AccuracyEnum.wrongPosition} position={2} value='A' />
-      <Letter accuracy={AccuracyEnum.wrongPosition} position={3} value='C' />
-      <Letter accuracy={AccuracyEnum.correct} position={4} value='T' />
+      <WordEntry onGuessEntered={(guess) => setWordGuess(guess)} />
+      <Word isWordEvaluated={false} guessWordValue={wordGuess} />
     </div>
   );
 }
